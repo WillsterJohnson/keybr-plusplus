431730b560939452486a38783edea2514a73d860
WillsterJohnson <willster@willsterjohnson.com>
2023-07-06 16:29:34 +0100

    :sparkles: Add capslock indicator

---
+++ src/contentscript/applets/lockIndicators.ts
@@ -0,0 +1,51 @@
+import { applet } from "../applet.js";
+
+class LockIndicator {
+  constructor() {
+    window.addEventListener("keydown", (event) => {
+      if (event.key === "CapsLock") {
+        this.setLightState(!this.capsLocked);
+      } else {
+        this.setLightState(event.getModifierState("CapsLock"));
+      }
+    });
+  }
+
+  protected capsLocked = false;
+
+  protected setLightState(state: boolean): void {
+    this.capsLocked = state;
+    const light = this.capsLight;
+    if (!light) return;
+    light.setAttribute(
+      "fill",
+      state ? "var(--LessonKey--fast__color)" : "currentColor",
+    );
+  }
+
+  protected get capsLight() {
+    const capsLock =
+      document.querySelector<SVGElement>("svg[data-key='CapsLock']") ??
+      undefined;
+    if (!capsLock) return;
+
+    const capsLockLight = capsLock.querySelector<SVGElement>(".indicator");
+    if (capsLockLight) return capsLockLight;
+
+    const newCapsLockLight = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "rect",
+    );
+    newCapsLockLight.setAttribute("class", "indicator");
+    newCapsLockLight.setAttribute("width", "5");
+    newCapsLockLight.setAttribute("height", "5");
+    newCapsLockLight.setAttribute("x", "5");
+    newCapsLockLight.setAttribute("y", "5");
+    newCapsLockLight.setAttribute("fill", "currentColor");
+
+    capsLock.appendChild(newCapsLockLight);
+    return newCapsLockLight;
+  }
+}
+
+export const lockIndicator = applet(["/"], LockIndicator);
